088ae2065e0b42350d4a858d8f570216de6aaa7d
agajardo100 <andresegajardo@gmail.com>
2017-02-26 01:15:11 -0500

    remove gzip

--- andresblog/settings/production.py
+++ andresblog/settings/production.py
@@ -124,8 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE ='django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
