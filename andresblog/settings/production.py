f38a78ef041ee70e1ad06db2d3d707d3fdeb72ab
agajardo100 <andresegajardo@gmail.com>
2017-02-26 01:19:07 -0500

    settings revert

--- andresblog/settings/production.py
+++ andresblog/settings/production.py
@@ -91,7 +91,6 @@ DATABASES = {
 }
 
 
-
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
@@ -124,8 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
+
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #STATICFILES_STORAGE ='django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
--- andresblog/wsgi.py
+++ andresblog/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-from settings.production import STATIC_ROOT
 from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "andresblog.settings.production")
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application, root=STATIC_ROOT)
+application = DjangoWhiteNoise(application)
